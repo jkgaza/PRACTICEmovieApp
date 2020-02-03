16e56f8bc0f729a97be46257b135e9befdd29033
Jordan Gaza <Gaza>
2020-02-02 20:04:50 -0500

    HOLY SHIT LITERALLY WAS JUST MISSING ONE LETTER IN THE URL AND SUDDENLY THE API IS BEING CALLED YEEEEAAAHH

--- src/app/components/movie-list/movie-list.component.ts
+++ src/app/components/movie-list/movie-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from "@angular/core";
+import { MoviesService } from 'src/app/services/movies.service';
 
 @Component({
   selector: "app-movie-list",
@@ -8,28 +9,9 @@ import { Component, OnInit } from "@angular/core";
 export class MovieListComponent implements OnInit {
   movies: any[];
 
-  constructor() {}
+  constructor(private service: MoviesService) {}
 
   ngOnInit() {
-    this.movies = [
-      {
-        Title: "Iron Man",
-        Year: "2008",
-        Poster:
-          "https://4.bp.blogspot.com/_U-_MB3ct83I/S6pL1KH_aHI/AAAAAAAAFI4/xAg9nivP8Bs/s1600/hr_iron_man_poster1.jpg"
-      },
-      {
-        Title: "Iron Man 2",
-        Year: "2010",
-        Poster:
-          "https://vignette.wikia.nocookie.net/transcripts/images/1/14/Marvel%27s_Iron_Man_2_-_iTunes_Movie_Poster.jpeg/revision/latest?cb=20170206025128"
-      },
-      {
-        Title: "Iron Man",
-        Year: "2012",
-        Poster:
-          "http://l.yimg.com/os/251/2013/02/27/ironman3-poster-watermark-jpg_162144.jpg"
-      }
-    ];
+  this.service.searchMovies('spider').subscribe(resp=>this.movies=resp.Search);
   }
 }
